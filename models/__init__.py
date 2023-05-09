b0b4db6df369bd6c699c0282f1196c7e830709db
dh031200 <imbird0312@gmail.com>
2023-05-09 19:03:29 +0900

    add `pyproject.toml`

--- models/__init__.py
+++ models/__init__.py
@@ -6,10 +6,9 @@ import gdown
 import torch
 import numpy as np
 import torch.nn.functional as F
-from yaml import safe_load
 from torchvision.transforms.functional import normalize
 
-from models.isnet import ISNetGTEncoder, ISNetDIS
+from models.isnet import ISNetDIS
 
 model = "isnet-general-use.pth"
 
@@ -53,18 +52,14 @@ def pre_processing(image):
     if len(image.shape) < 3:
         image = image[:, :, np.newaxis]
     im_tensor = torch.tensor(image, dtype=torch.float32).permute(2, 0, 1)
-    im_tensor = F.interpolate(
-        torch.unsqueeze(im_tensor, 0), [1024, 1024], mode="bilinear"
-    ).type(torch.uint8)
+    im_tensor = F.interpolate(torch.unsqueeze(im_tensor, 0), [1024, 1024], mode="bilinear").type(torch.uint8)
     image = torch.divide(im_tensor, 255.0)
     image = normalize(image, [0.5, 0.5, 0.5], [1.0, 1.0, 1.0])
     return image
 
 
 def post_processing(result, origin):
-    result = torch.squeeze(
-        F.interpolate(result[0][0], origin.shape[:2], mode="bilinear"), 0
-    )
+    result = torch.squeeze(F.interpolate(result[0][0], origin.shape[:2], mode="bilinear"), 0)
     ma = torch.max(result)
     mi = torch.min(result)
     result = (result - mi) / (ma - mi)
