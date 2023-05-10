05a4c8fbb2aeb1c52671ce30bc5252df42a945ed
dh031200 <imbird0312@gmail.com>
2023-05-10 13:54:46 +0900

    add python usage

--- src/dis_inference/__init__.py
+++ src/dis_inference/__init__.py
@@ -1,3 +1,7 @@
 # SPDX-FileCopyrightText: 2023-present dh031200 <imbird0312@gmail.com>
 #
 # SPDX-License-Identifier: AGPL-3.0-only
+from dis_inference.cli import inference
+from dis_inference.__about__ import __version__
+
+__all__ = 'inference', '__version__'
--- src/dis_inference/models/isnet.py
+++ src/dis_inference/models/isnet.py
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: 2023-present Xuebin Qin <xuebin@ualberta.ca>
+#
+# SPDX-License-Identifier: Apache-2.0
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
